1ea6de673c6fa5b790fdb09ad7b1a4676838a196
a-ikuma <akinobu.ikuma@kikagaku.co.jp>
2024-04-15 15:11:25 +0900

    1st commmit

--- iris.py
+++ iris.py
@@ -1,10 +1,14 @@
 # 基本ライブラリ
+
+"""
+なんかエラー出る．
+pandasのバージョンが関係しているっぽい
+"""
 import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.datasets import load_iris
-
 
 # データセット読み込み
 iris = load_iris()
@@ -18,9 +22,9 @@ df.loc[df['target'] == 0, 'target'] = 'setosa'
 df.loc[df['target'] == 1, 'target'] = 'versicolor'
 df.loc[df['target'] == 2, 'target'] = 'virginica'
 
-
 # 予測モデル構築
 x = iris.data[:, [0, 2]] 
+#print(x) データ読み込めているかの確認
 y = iris.target
 
 # ロジスティック回帰
@@ -40,8 +44,15 @@ st.write('## Input Value')
 # インプットデータ（1行のデータフレーム）
 value_df = pd.DataFrame([],columns=['data','sepal length (cm)','petal length (cm)'])
 record = pd.Series(['data',sepalValue, petalValue], index=value_df.columns)
-value_df = value_df.append(record, ignore_index=True)
+#############################
+#変更
+#value_df = value_df.append(record, ignore_index=True)
+value_df = pd.concat([value_df, pd.DataFrame([record])], ignore_index=True)
+#############################
 value_df.set_index('data',inplace=True)
+
+
+
 
 # 入力値の値
 st.write(value_df)
@@ -56,4 +67,4 @@ st.write(pred_df)
 # 予測結果の出力
 name = pred_df.idxmax(axis=1).tolist()
 st.write('## Result')
-st.write('このアヤメは',str(name[0]),'だと予測されます')
+st.write('このアヤメは',str(name[0]),'だと予想されます')
